d9ec9534a2e544d6767b68016c106f1bcd7eece0
matiwang <matiwang16@gmail.com>
2024-10-08 15:40:50 -0300

    Cree funcion hashing e implementacion hash abierto

--- tdas/diccionario/hash.go
+++ tdas/diccionario/hash.go
@@ -1,3 +1,37 @@
 package diccionario
 
+import (
+	"fmt"
+	TDALista "tdas/lista"
+)
+const TAM_INICIAL = 5
+
+type parClaveValor[K comparable, V any] struct {
+	clave K
+	valor V
+}
+type hashAbierto[K comparable, V any] struct {
+	tabla []TDALista.Lista[parClaveValor[K, V]]
+	tam int
+	cantidad int
+}
+
+func convertirABytes[K comparable](clave K) []byte {
+	return []byte(fmt.Sprintf("%v", clave))
+}
+
+//Suma los valores de los bytes y calcula el módulo del tamaño de la tabla
+func hashingFuncion[K comparable](clave K, tam int) int {
+	bytes := convertirABytes(clave)
+	suma := 0
+	for _, b := range bytes {
+		suma += int(b)
+	}
+	return suma % tam
+}
+
+func CrearHash[K comparable, V any]() Diccionario[K, V] {
+	//
+}
+
 
