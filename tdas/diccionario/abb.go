8b91cfce1f8dd8eab70c6b3b44d7cb0039b23485
matiwang <matiwang16@gmail.com>
2024-10-22 12:45:55 -0300

    Cree buscar e implemente algunas primitivas

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -28,3 +28,36 @@ func crearAbb[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdena
 	return abb
 }
 
+func (a *abb[K, V]) buscar(clave K, nodo *nodoAbb[K, V]) *nodoAbb[K, V] {
+	if nodo == nil {
+		return nodo
+	}
+	comparacion := a.cmp(clave, nodo.clave)
+
+	if comparacion == 0 {
+		return nodo
+	}else if comparacion < 0 {
+		return a.buscar(clave, nodo.izquierdo)
+	}else {
+		return a.buscar(clave, nodo.derecho)
+	}
+}
+
+func (a *abb[K, V]) Pertenece(clave K) bool {
+	return a.buscar(clave, a.raiz) != nil
+}
+
+func (a *abb[K, V]) Obtener(clave K) V {
+	nodo := a.buscar(clave, a.raiz)
+
+	if nodo == nil {
+		panic("La clave no pertenece al diccionario")
+	}
+
+	return nodo.dato
+}
+
+func (a *abb[K, V]) Cantidad() int {
+	return a.cantidad
+}
+
