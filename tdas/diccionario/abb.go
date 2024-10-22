d47ac6e26465d373aec42c9722b9005002cf7e41
matiwang <matiwang16@gmail.com>
2024-10-21 22:07:03 -0300

    implemente funciones Crear nodo y abb

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -15,3 +15,16 @@ type abb[K comparable, V any] struct {
 	cmp      funcCmp[K]
 }
 
+func crearNodoAbb[K comparable, V any]() *nodoAbb[K, V] {
+	nodo := new(nodoAbb[K, V])
+	return nodo
+}
+
+func crearAbb[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdenado[K, V] {
+	abb := new(abb[K, V])
+	var raiz *nodoAbb[K, V]
+	abb.raiz = raiz
+	abb.cmp = funcion_cmp
+	return abb
+}
+
