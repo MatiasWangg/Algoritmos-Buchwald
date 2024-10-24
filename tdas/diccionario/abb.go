0d0ebb460d551c6220f32368cebd3613eb02a298
matiwang <matiwang16@gmail.com>
2024-10-23 22:48:53 -0300

    Implemente y cambie primitivas

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -28,27 +28,28 @@ func crearAbb[K comparable, V any](funcion_cmp func(K, K) int) DiccionarioOrdena
 	return abb
 }
 
-func (a *abb[K, V]) buscar(clave K, nodo *nodoAbb[K, V]) *nodoAbb[K, V] {
+func (a *abb[K, V]) buscar(clave K, nodo *nodoAbb[K, V], padre *nodoAbb[K, V]) (*nodoAbb[K, V], *nodoAbb[K, V]) {
 	if nodo == nil {
-		return nodo
+		return nodo, padre
 	}
 	comparacion := a.cmp(clave, nodo.clave)
 
 	if comparacion == 0 {
-		return nodo
+		return nodo, padre
 	}else if comparacion < 0 {
-		return a.buscar(clave, nodo.izquierdo)
+		return a.buscar(clave, nodo.izquierdo, nodo)
 	}else {
-		return a.buscar(clave, nodo.derecho)
+		return a.buscar(clave, nodo.derecho, nodo)
 	}
 }
 
 func (a *abb[K, V]) Pertenece(clave K) bool {
-	return a.buscar(clave, a.raiz) != nil
+	nodo, _ := a.buscar(clave, a.raiz, nil)
+	return nodo != nil
 }
 
 func (a *abb[K, V]) Obtener(clave K) V {
-	nodo := a.buscar(clave, a.raiz)
+	nodo, _ := a.buscar(clave, a.raiz, nil)
 
 	if nodo == nil {
 		panic("La clave no pertenece al diccionario")
@@ -61,3 +62,80 @@ func (a *abb[K, V]) Cantidad() int {
 	return a.cantidad
 }
 
+func (a *abb[K, V]) Guardar(clave K, dato V) {
+	nodo, padre := a.buscar(clave, a.raiz, nil)
+
+	if nodo != nil {
+		nodo.dato = dato
+	} else {
+		nuevoNodo := crearNodoAbb[K, V]() 
+		nuevoNodo.clave = clave
+		nuevoNodo.dato = dato
+
+		if padre != nil { 
+			comparacion := a.cmp(clave, padre.clave)
+			if comparacion < 0 {
+				padre.izquierdo = nuevoNodo
+			} else {
+				padre.derecho = nuevoNodo
+			}
+		} else {
+			a.raiz = nuevoNodo 
+		}
+
+		a.cantidad++
+	}
+}
+
+func (a *abb[K, V]) Borrar(clave K) V {
+	nodo, padre := a.buscar(clave, a.raiz, nil)
+	if nodo == nil {
+		panic("La clave no pertenece al diccionario")
+	}
+
+	dato := nodo.dato 
+
+	if nodo.izquierdo == nil && nodo.derecho == nil {
+		if padre != nil {
+			if a.cmp(clave, padre.clave) < 0 {
+				padre.izquierdo = nil
+			} else {
+				padre.derecho = nil
+			}
+		} else {
+			a.raiz = nil
+		}
+	} else if nodo.izquierdo == nil || nodo.derecho == nil {
+		var hijo *nodoAbb[K, V]
+		if nodo.izquierdo != nil {
+			hijo = nodo.izquierdo
+		} else {
+			hijo = nodo.derecho
+		}
+
+		if padre != nil {
+			if a.cmp(clave, padre.clave) < 0 {
+				padre.izquierdo = hijo
+			} else {
+				padre.derecho = hijo
+			}
+		} else {
+			a.raiz = hijo
+		}
+	} else {
+		sustituto := a.encontrarMinimo(nodo.derecho)
+		nodo.clave = sustituto.clave
+		nodo.dato = sustituto.dato
+		a.Borrar(sustituto.clave) 
+	}
+
+	a.cantidad--
+	return dato
+}
+
+func (a *abb[K, V]) encontrarMinimo(nodo *nodoAbb[K, V]) *nodoAbb[K, V] {
+	for nodo.izquierdo != nil {
+		nodo = nodo.izquierdo
+	}
+	return nodo
+}
