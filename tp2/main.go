166c0334c0f467f05a1d168976fd0dc83a469f81
matiwang <matiwang16@gmail.com>
2024-11-15 12:47:22 -0300

    Comienzo lectura entrada main.go

--- tp2/main.go
+++ tp2/main.go
@@ -1,1 +1,27 @@
 package tp2
+
+import(
+	"bufio"
+	"os"
+	"fmt"
+)
+
+//Estructura inicial, nada definitivo 
+func main(){
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		comando := scanner.Text()
+
+
+		err := procesarComando(......)
+		if err != nil{
+			fmt.Println(os.Stderr,"Error en el comando ingresado")
+		}
+	}
+}
+
+
+
+//Pensaba hacer  una funcion con un switch para procesar cada comando recibido e ir
+//llamando a las diferentes funciones que estaran en otros archivos
