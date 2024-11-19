049e75aac583187b6fd98bef870441c77f6a1926
matiwang <matiwang16@gmail.com>
2024-11-19 15:38:38 -0300

    Complete VerVisitantes

--- tp2/verficarVisitantes.go
+++ tp2/verficarVisitantes.go
@@ -1,13 +1,26 @@
 package tp2
 
 import (
+	"fmt"
 	"tdas/diccionario"
 )
 
 func VerVisitantes(desde, hasta string, visitantes diccionario.DiccionarioOrdenado[int, string]) {
+	ipDesde := conversionIP(desde)
+	ipHasta := conversionIP(hasta)
 
+	fmt.Println("Visitantes:")
+	iter := visitantes.Iterador()
+	for iter.HaySiguiente(){
+		ipNumerica, ip := iter.VerActual()
+		if ipNumerica >= ipDesde && ipNumerica <= ipHasta {
+			fmt.Printf("\t%s\n",ip)
+		}
+		iter.Siguiente()
+	}
+	fmt.Println("OK")
 }
 
 func VerMasVisitados(n int, recursos diccionario.Diccionario[string, int]) {
-
+Ver
 }
