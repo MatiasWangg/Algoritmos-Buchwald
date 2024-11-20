d7f42ec99407769d61992e84741711b4d85d7306
NkofR <ijrodriguez@fi.uba.ar>
2024-11-20 17:51:27 -0300

    Cambio la disposicion de archivos

--- tp2/main.go
+++ tp2/main.go
@@ -1,4 +1,4 @@
-package tp2
+package main
 
 import (
 	"bufio"
@@ -6,18 +6,19 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"tdas/diccionario"
+	TDADicc "tdas/diccionario"
+	LOG "tp2/log"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	//Estructuras para guardar los datos
-	visitantes := diccionario.CrearABB[int, string](compararInts)
+	visitantes := TDADicc.CrearABB[int, string](compararInts)
 	//visitantes debe ser un arbol para iterarlo con desde y hasta
 	//la clave es un int que sera la representacion numerica
 	//y el valor deberia guardar la ip
-	recursos := diccionario.CrearHash[string, int]()
+	recursos := TDADicc.CrearHash[string, int]()
 
 	for scanner.Scan() {
 		comando := scanner.Text()
@@ -31,25 +32,25 @@ func main() {
 
 // Pensaba hacer  una funcion con un switch para procesar cada comando recibido e ir
 // llamando a las diferentes funciones que estaran en otros archivos
-func procesarComando(comando string, visitantes diccionario.DiccionarioOrdenado[int, string], recursos diccionario.Diccionario[string, int]) error {
+func procesarComando(comando string, visitantes TDADicc.DiccionarioOrdenado[int, string], recursos TDADicc.Diccionario[string, int]) error {
 	partes := strings.Fields(comando)
 
 	switch partes[0] {
 	case "agregar_archivo":
 		archivo := partes[1]
 		//funcion que trabaja con los log (parametros provisorios)
-		return AgregarArchivo(archivo, visitantes, recursos)
+		return LOG.AgregarArchivo(archivo, visitantes, recursos)
 	case "ver_visitantes":
 		desde, hasta := partes[1], partes[2]
 		//Lo mismo aca con los parametros
-		VerVisitantes(desde, hasta, visitantes)
+		LOG.VerVisitantes(desde, hasta, visitantes)
 	case "ver_mas_visitados":
 		n, err := strconv.Atoi(partes[1])
 		//Lo mismo aca con los parametros
 		if err != nil {
 			return fmt.Errorf("error en ver_mas_visitados")
 		}
-		VerMasVisitados(n, recursos)
+		LOG.VerMasVisitados(n, recursos)
 	default:
 		return fmt.Errorf("comando no reconocido")
 	}
